9767db3389fcdae23c0e04853a99ecd6f806fa98
RMT <iamrmttt@gmail.com>
2026-03-15 20:32:21 +0800

    WIP

--- tests/e2e/main_test.go
+++ tests/e2e/main_test.go
@@ -1,13 +1,20 @@
 package e2e
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/exec"
 	"testing"
+
+	"github.com/testcontainers/testcontainers-go/network"
 )
 
+var SharedNetworkName string
+
 func TestMain(m *testing.M) {
+	ctx := context.Background()
+
 	// Compile the gost binary
 	cmd := exec.Command("go", "build", "-o", "/tmp/gost-test-bin", "../../cmd/gost")
 	cmd.Env = append(os.Environ(), "CGO_ENABLED=0")
@@ -17,12 +24,23 @@ func TestMain(m *testing.M) {
 		fmt.Printf("Failed to compile gost: %v\n", err)
 		os.Exit(1)
 	}
+	defer func() {
+		os.Remove("/tmp/gost-test-bin")
+	}()
+
+	// Create a shared Docker network
+	net, err := network.New(ctx)
+	if err != nil {
+		fmt.Printf("Failed to create network: %v\n", err)
+		os.Exit(1)
+	}
+	SharedNetworkName = net.Name
 
 	// Run tests
 	code := m.Run()
 
 	// Cleanup
-	os.Remove("/tmp/gost-test-bin")
+	net.Remove(ctx)
 
 	os.Exit(code)
 }
